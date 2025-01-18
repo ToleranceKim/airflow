ae3bbc59a96ce42742b018575d36d18d0b6042eb
ToleranceKim <kwanyou2@gmail.com>
2025-01-18 16:14:06 +0900

    파이썬 데코레이터 dags 수정

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -13,4 +13,4 @@ with DAG(
     def print_context(some_input):
         print(some_input)
     
-    python_task_1 = print(context('task_decorator 실행'))
+    python_task_1 = print_context('task_decorator 실행')
